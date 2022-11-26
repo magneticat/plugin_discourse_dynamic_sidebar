7e04127cc71d107941778f38d84553bcb55085fd
MagnetiCat <development@magneticat.com>
2022-11-26 18:54:34 +0100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -2,6 +2,5 @@
 # about: adds a dynamic sidebar on the right of the pages
 # authors: Vairix, Arpit Jalan, MagnetiCat
 
-register_asset "javascripts/admin/templates/site_settings/setting_text.hbs"
 register_asset "stylesheets/sidebar_styles.css"
 
